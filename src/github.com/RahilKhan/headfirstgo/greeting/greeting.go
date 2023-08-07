3b035d4d3cb724ab0b2a07e9c15180917bcd2107
Rahil Khan <rashidali.rahil@gmail.com>
2023-08-07 11:34:54 +0530

    Added comments

--- src/github.com/RahilKhan/headfirstgo/greeting/greeting.go
+++ src/github.com/RahilKhan/headfirstgo/greeting/greeting.go
@@ -1,11 +1,14 @@
+// Provides differen salutation methods
 package greeting
 
 import "fmt"
 
+// Greets with formal -> hello
 func Hello() {
 	fmt.Println("Hello !!!")
 }
 
+// Greets with informal/casual -> hi
 func Hi() {
 	fmt.Println("Hi !!!")
 }
